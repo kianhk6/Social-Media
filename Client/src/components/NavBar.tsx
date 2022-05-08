ac1da12c20663bccc805ba65e6fbb8849c38acf1
Kian Hosseinkhani <kianhk6@gmail.com>
2022-05-08 13:59:43 -0700

    caching for logged in user is done v0.0

--- Client/src/components/NavBar.tsx
+++ Client/src/components/NavBar.tsx
@@ -1,20 +1,45 @@
-import { Box, Flex, Link } from '@chakra-ui/react';
+import { Box, Button, Flex, Link } from '@chakra-ui/react';
 import React from 'react'
 import NextLink from 'next/link'; //for linking to pages navigation
+import { useMeQuery } from '../generated/graphql';
 interface NavBarProps {
 
 }
 
 export const NavBar: React.FC<NavBarProps> = ({ }) => {
-    return (
-        <Flex bg="gray.700" p={4}>
-            <Box ml={"auto"}>
+    const [{ data, fetching }] = useMeQuery();
+    let body = null;
+    //data is loading
+    if (fetching) {
+        body = null;
+    }
+    //user not logged in
+    else if (!data?.me) {
+        body = ( //what will be displayed
+            <>
                 <NextLink href="/login">
                     <Link color="whiteAlpha.900" mr={2}>Login</Link>
                 </NextLink>
                 <NextLink href="/register">
                     <Link color="whiteAlpha.900">Register</Link>
                 </NextLink>
+            </>
+        )
+    }
+    //user logged in
+    else {
+        body = (
+            <Flex>
+                <Box mr={2} color="whiteAlpha.900">{data.me.username}</Box>
+                <Box><Button color="whiteAlpha.900" variant="link">Logout</Button></Box>
+            </Flex>
+
+        )
+    }
+    return (
+        <Flex bg="gray.700" p={4}>
+            <Box ml={"auto"}>
+                {body}
             </Box>
         </Flex>
     );
--- Client/src/pages/_app.tsx
+++ Client/src/pages/_app.tsx
@@ -1,13 +1,72 @@
-import { ChakraProvider } from '@chakra-ui/react'
-
+import { ChakraProvider, InputGroup } from '@chakra-ui/react'
 import theme from '../theme'
 import { AppProps } from 'next/app'
-import { Provider, createClient } from 'urql'
+import { Provider, createClient, dedupExchange, fetchExchange } from 'urql'
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache'; //for custom updates 
+import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from '../generated/graphql';
+import e from 'cors';
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, data => fn(result, data as any) as any);
+}
+
 const client = createClient({
   url: 'http://localhost:4000/graphql',
-  fetchOptions:{
+  fetchOptions: {
     credentials: "include", //actually sends the cookie 
-  }
+  },
+  // @ts-ignore
+  // this is for caching when we logged in as a middleware 
+  // (we login as the actual acc that just logged in)
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (_result, args, cache, info) => {
+          //update the query of what we are getting which is a login query that returns user
+          //if successfull (second else)
+          betterUpdateQuery<LoginMutation, MeQuery>(
+            cache,
+            { query: MeDocument },
+            _result, 
+            (result, query) => {
+              if (result.login.errors) {
+                return query; //if there was a error through our result of query
+              }
+              else {
+                return {
+                  me: result.login.user, //if there was none there must be user so return it
+                }
+              }
+
+
+            }
+          );
+        },
+        register: (_result, args, cache, info) => {
+          betterUpdateQuery<RegisterMutation, MeQuery>(cache,
+            { query: MeDocument },
+            _result,
+            (result, query) => {
+              if (result.register.errors) {
+                return query;
+              }
+              else {
+                return {
+                  me: result.register.user,
+                }
+              }
+            }
+          );
+        },
+      },
+    },
+  }), fetchExchange],
+  // exchanges: [dedupExchange, cacheExchange({}), fetchExchange]
 });
 function MyApp({ Component, pageProps }: AppProps) {
   return (
